c1474fe05c3b5226452676f708c5f16d7658f231
DMGamage <gamagemadhushan@gmail.com>
2024-06-15 21:22:18 +0530

    Docker App Create

--- app/test_endpoints.py
+++ app/test_endpoints.py
@@ -9,8 +9,3 @@ def test_get_home():
     assert "text/html" in response.headers["Content-Type"]
 
 
-def test_Post_home():
-    response = client.post("/")
-    assert response.status_code == 200
-    assert "application/json" in response.headers["Content-Type"]
-    assert response.json() == {"hello":"world"}
